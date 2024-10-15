0e04d4f36b4583623277fcb875dad6df6762b7d7
Pritam <pritamsengupta18@gmail.com>
2024-10-15 17:10:27 +0530

    Updated code with the latest changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
 
         {/* CV Button */}
         <a
-          href="./assets/resume3.pdf.pdf"
+          href="./assets/resume3.pdf"
           download="Pritam_Sengupta_CV.pdf"
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded blink-animation"
         >
